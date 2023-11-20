e87c01bcafb589d536fbbc764de35959ae222721
YFisch <yfisch381@gmail.com>
2023-11-20 14:45:25 -0500

    Connect Four Web with jQuery

---
+++ ConnectFour/ConnectFourWeb/Pages/PlayConnectFourjQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConnectFourWeb.Pages
+{
+    public class PlayConnectFourjQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
