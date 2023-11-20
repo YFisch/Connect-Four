1f442792cba5898e6caab468141d28f71227de80
YFisch <yfisch381@gmail.com>
2023-11-20 11:34:43 -0500

    Connect Four Web

---
+++ ConnectFour/ConnectFourWeb/Pages/PlayConnectFour.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConnectFourWeb.Pages
+{
+    public class PlayConnectFourModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
